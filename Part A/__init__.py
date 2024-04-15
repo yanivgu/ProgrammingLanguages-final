bc81a1a0665ddcf21ebce012d34832460a40d6d1
Yaniv Gutman <ygutman1@gmail.com>
2024-04-15 22:26:26 +0300

    organize

---
+++ Part A/__init__.py
@@ -0,0 +1,10 @@
+# This is the operators package
+
+# Import any modules or sub-packages here
+from interpreter import interpret
+
+# Define any global variables or constants here
+
+# Define any functions or classes here
+
+# You can also add any initialization code or comments as needed
--- Part A/main.py
+++ Part A/main.py
@@ -1,35 +1,4 @@
-from data_types import Integer, Variable
-from commands import Print
-from operators import Add, Sub, Mul, Div, Equal
-
-def interpret(code):
-    code = code.strip()
-    if Print.check_prefix(code):
-        if not Print.check_command(code):
-            raise ValueError("Invalid print statement", code)
-        internal_expression = Print.extract_expression(code)
-        return Print(interpret(internal_expression))
-    elif Equal.check_expression(code):
-        parts = Equal.split(code)
-        return Equal(interpret(parts[0]), interpret(parts[1]))
-    elif Add.check_expression(code):
-        parts = Add.split(code)
-        return Add(interpret(parts[0]), interpret(parts[1]))
-    elif Sub.check_expression(code):
-        parts = Sub.split(code)
-        return Sub(interpret(parts[0]), interpret(parts[1]))
-    elif Mul.check_expression(code):
-        parts = Mul.split(code)
-        return Mul(interpret(parts[0]), interpret(parts[1]))
-    elif Div.check_expression(code):
-        parts = Div.split(code)
-        return Div(interpret(parts[0]), interpret(parts[1]))
-    elif Integer.check_expression(code):
-        return Integer(code)
-    elif Variable.check_expression(code):
-        return Variable(code)
-    else:
-        raise ValueError("Invalid operator", code)
+from interpreter import *
 
 code = ""
 while (code.strip() != "exit"):
