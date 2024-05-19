7f0342ec1c76c66323ce16616f3bfbaf74b2d76c
Yaniv Gutman <ygutman1@gmail.com>
2024-05-19 11:02:22 +0300

    fix while

--- Part A/interpreter.py
+++ Part A/interpreter.py
@@ -43,7 +43,7 @@ def __interpret(line: str, recurse_level, line_number):
             if top_while_scope is not None and top_while_scope.is_first_pass():
                 for loop_line in while_scope.get_lines():
                     top_while_scope.add_line(loop_line)
-                top_while_scope.add_line('endwhile')
+                #top_while_scope.add_line('endwhile')
         
     if scope.is_skipping_if():
         if EndIf.check_command(line):
